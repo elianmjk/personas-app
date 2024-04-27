2286b0cba37020801465143cd95f81e7524675e1
elianmjk <elian.10lyn@gmail.com>
2024-04-27 16:17:15 -0500

    Volviendo hacer el trabajo

---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Http\Controllers\ComunaController;
+use Illuminate\Support\Facades\Route;
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/comunas ',[ComunaController::class,'index'])->name('comunas.index');
+Route::get('/comunas/create ',[ComunaController::class,'create'])->name('comunas.create');
+
+
